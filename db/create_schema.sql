8714b9ff510bac31522d5af7cabab5c3aba84553
LyRv1 <leydi.riv1@gmail.com>
2024-10-27 21:35:52 -0600

    Initial commit

---
+++ db/create_schema.sql
@@ -0,0 +1,16 @@
+create database checkDatabase;
+
+/*drop database checkDatabase; */
+use checkDatabase;
+
+show tables;
+
+select * from employee;
+select * from check_in;
+select * from department;
+select * from position;
+
+describe employee;
+explain department;
+explain position;
+
